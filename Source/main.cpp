8bdb0bb4c674926550cca62eedc5f07e5c753c1c
Salicorne <timothee.raboeuf@gmail.com>
2018-05-02 11:57:12 +0200

    Cleaned main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,15 +1,5 @@
 #include "Screen.h"
 
-
-/*void render(sf::RenderWindow& window, MemoryManager& mgr) {
-	while (window.isOpen()) {
-		window.clear();
-		mgr.draw(window);
-		window.display();
-		sf::sleep(sf::milliseconds(10));
-	}
-}*/
-
 void animate(sf::RenderWindow& window, MemoryManager& mgr, GameManager& gmgr) {
 	sf::Clock clock;
 	while (window.isOpen()) {
@@ -50,7 +40,6 @@ int main()
 	srand(time(NULL));
 
 	sf::RenderWindow window(sf::VideoMode(Game::Window::Width, Game::Window::Height), "SFML works!", sf::Style::Titlebar | sf::Style::Close);
-	//sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "SFML works!", sf::Style::Fullscreen);
 	window.setActive(false);
 	window.setKeyRepeatEnabled(false);
 	window.setMouseCursorVisible(true);
@@ -82,12 +71,8 @@ int main()
 		return -1;
 	}
 
-	//std::thread renderThread(render, std::ref(window), std::ref(mgr));
 	std::thread animateThread(animate, std::ref(window), std::ref(mgr), std::ref(gmgr));
 	std::thread actionThread(action, std::ref(window), std::ref(mgr), std::ref(gmgr));
-
-	//sf::Thread renderThread(std::bind(render, std::ref(window), std::ref(mgr)));
-	//renderThread.launch();
 
 	sf::Clock clock;
 	vec2f playerMovement(0, 0);
@@ -95,61 +80,10 @@ int main()
 		screens[Game::currentScreen]->setUp();
 		Game::currentScreen = screens[Game::currentScreen]->run(mgr, gmgr, window);
 	}
-	/*while (window.isOpen())
-	{
-		sf::Event event;
-		sf::Time elapsed = clock.restart();
-		while (window.pollEvent(event))
-		{
-			gmgr.getDesktop().HandleEvent(event);
-
-			if (event.type == sf::Event::Closed)
-				window.close();
-
-			if (event.type == sf::Event::KeyPressed) {
-				if (event.key.code == sf::Keyboard::Escape) {
-					window.close();
-				}
-				if (event.key.code == sf::Keyboard::P) {
-					gmgr.showMessage("Magicien", "Bienvenue, jeune homme...");
-				}
-			}
-		}
-		gmgr.getDesktop().Update(elapsed.asSeconds());
-
-
-		playerMovement = vec2f(0, 0);
-
-		sf::Joystick::update();
-
-		if (Game::joystick != -1 && sf::Joystick::isConnected(Game::joystick)) {
-			playerMovement.x = sf::Joystick::getAxisPosition(Game::joystick, sf::Joystick::Axis::X)/100;
-			playerMovement.y = sf::Joystick::getAxisPosition(Game::joystick, sf::Joystick::Axis::Y)/100;
-		}
-		else {
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) { playerMovement.y--; }
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) { playerMovement.x--; }
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) { playerMovement.y++; }
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) { playerMovement.x++; }
-		}
-
-
-		gmgr.movePlayer(playerMovement, elapsed);
-
-		window.clear();
-		mgr.draw(window);
-		gmgr.drawGUI(window);
-		window.display();
-
-		sf::sleep(sf::milliseconds(10));
-	}*/
 
 	window.close();
-	///renderThread.join();
 	animateThread.join();
 	actionThread.join();
-
-	//renderThread.wait();
 
 	return 0;
 }
