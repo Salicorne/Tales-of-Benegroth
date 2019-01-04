26a7509149ba614c87cfe01ab7bc0964c8cb0180
Salicorne <timothee.Raboeuf@gmail.com>
2019-01-04 17:58:23 +0100

    First demo commit

--- Source/Scene.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Game.h"
-
-class Scene {
-    public:
-        static bool loadScene(std::string filePath, MemoryManager& mgr, GameManager& gmgr);
-};
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,122 +1,27 @@
-#include "Screen.h"
-
-void animate(sf::RenderWindow& window, MemoryManager& mgr, GameManager& gmgr) {
-	sf::Clock clock;
-	while (window.isOpen()) {
-		sf::Time elapsed = clock.restart();
-		if(Game::currentScreen == Game::Screen::Game) {
-			mgr.animateSprites(elapsed);
-			mgr.updateBlur(gmgr.getPlayer()->getSpriteSet()->getFeetPos());
-		}
-		sf::sleep(sf::milliseconds(50));
-	}
-}
-
-void action(sf::RenderWindow& window, MemoryManager& mgr, GameManager& gmgr) {
-	sf::Clock clock;
-	while (window.isOpen()) {
-		sf::Time elapsed = clock.restart();
-		if(Game::currentScreen == Game::Screen::Game) {
-			gmgr.playActors(elapsed);
-		}
-		sf::sleep(sf::milliseconds(20));
-	}
-}
-
-void updateActionMessage(sf::RenderWindow& window, GameManager& gmgr) {
-	while (window.isOpen()) {
-		if(Game::currentScreen == Game::Screen::Game) {
-			gmgr.updateActionMessage();
-		}
-		sf::sleep(sf::milliseconds(300));
-	}
-}
-
-void updateTimeOfDay(sf::RenderWindow& window, GameManager& gmgr) {
-	sf::Clock clock;
-	while (window.isOpen()) {
-		sf::Time t = clock.restart();
-		if(Game::currentScreen == Game::Screen::Game) {
-			gmgr.updateTimeOfDay(t.asMilliseconds() / 60000.0); // 1 min / sec
-		}
-		//std::cout << gmgr.getTimeOfDay() << std::endl;
-		sf::sleep(sf::milliseconds(100));
-	}
-}
-
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
 
 int main()
 {
-	std::cout << "Tales of Benegroth" << std::endl;
-	sfg::SFGUI gui;
-	Game::Window::Width = 853;
-	Game::Window::Height = 480;
-	Game::Window::widthScrollLimit = 0.1f;
-	Game::Window::heightScrollLimit = 0.1f;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-	sf::Font font;
-    if (!font.loadFromFile("Assets/Onciale PhF01.ttf"))
-        std::cout << "Could not load font." << std::endl;
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-	srand(time(NULL));
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
-	if(!sf::Shader::isAvailable()) {
-		std::cerr << "Shaders are not available. " << std::endl;
-		Game::canUseShaders = false;
-	}
-	else {
-		Game::canUseShaders = true;
-	}
-
-	sf::RenderWindow window(sf::VideoMode(Game::Window::Width, Game::Window::Height), "Tales of Benegroth - pre-alpha", sf::Style::Titlebar | sf::Style::Close);
-	window.setActive(false);
-	window.setKeyRepeatEnabled(false);
-	window.setMouseCursorVisible(true);
-
-	Game::Window::Width = window.getSize().x;
-	Game::Window::Height = window.getSize().y;
-
-	MemoryManager mgr;
-	GameManager gmgr(mgr, window);
-
-	Game::joystick = -1;
-	for (unsigned int i = 1; i< sf::Joystick::Count; ++i)
-	{
-		if (sf::Joystick::isConnected(i)) {
-			std::cout << "Joystick " << i << " is connected !" << std::endl;
-			Game::joystick = i;
-		}
-	}
-
-	std::vector<AbstractScreen*> screens;
-	MenuScreen menuScreen(gui);
-	screens.push_back(&menuScreen);
-	GameScreen gameScreen(gui);
-	screens.push_back(&gameScreen);
-	Game::currentScreen = 0;
-
-	if(!Scene::loadScene("save.dat", mgr, gmgr)) {
-		std::cerr << "Error loading main scene, aborting. " << std::endl;
-		return -1;
-	}
-
-	std::thread animateThread(animate, std::ref(window), std::ref(mgr), std::ref(gmgr));
-	std::thread actionThread(action, std::ref(window), std::ref(mgr), std::ref(gmgr));
-	std::thread actionMessageThread(updateActionMessage, std::ref(window), std::ref(gmgr));
-	std::thread timeOfDayThread(updateTimeOfDay, std::ref(window), std::ref(gmgr));
-
-	sf::Clock clock;
-	vec2f playerMovement(0, 0);
-	while(Game::currentScreen >= 0) {
-		screens[Game::currentScreen]->setUp(mgr, gmgr, window);
-		Game::currentScreen = screens[Game::currentScreen]->run(mgr, gmgr, window);
-	}
-
-	window.close();
-	timeOfDayThread.join();
-	actionMessageThread.join();
-	animateThread.join();
-	actionThread.join();
-
-	return 0;
+    return 0;
 }
+
